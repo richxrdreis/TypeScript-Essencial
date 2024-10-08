71aac0522f20717296aa1ca810329c8a3469940d
Richard Reis <167144386+richxrdreis@users.noreply.github.com>
2024-10-08 11:02:11 -0300

    classes

--- ts/src/classes/usuario.ts
+++ ts/src/classes/usuario.ts
@@ -1,1 +1,13 @@
-class Usuario {}
+class Usuario {
+    constructor(
+        public id: number,
+        public nome: string,
+        public email: string,
+        public ativo: boolean,
+        public senha: string | null = null,
+    ) {}
+}
+
+const usuario: Usuario = new Usuario(1, 'João', 'joao@empresa.com.br', true);
+usuario.nome = 'João da Silva';
+console.log(usuario.nome);
